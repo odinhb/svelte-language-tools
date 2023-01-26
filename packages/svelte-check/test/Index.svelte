85b8c115063ff142276b92f93d0a169cd4b3b31e
Odin H B <odinhb@protonmail.com>
2023-01-26 15:37:18 +0100

    add more example files for testing svelte-check

--- packages/svelte-check/test/Index.svelte
+++ packages/svelte-check/test/Index.svelte
@@ -1,6 +1,12 @@
 <script>
-    const a = true;
-    a === true;
+  import Untyped from "./Untyped.svelte";
+  import Typed from "./Typed.svelte";
+  import {jsFunction} from "./javascript";
+  import {tsFunction} from "./typescript";
+
+  import {jsxFunction} from "./untypedrendering";
+  import {tsxFunction} from "./typedrendering";
 </script>
 
-{a}
+<Untyped />
+<Typed />
---
+++ packages/svelte-check/test/untypedrendering.jsx
@@ -0,0 +1,10 @@
+import h from "./fakeJsxProvider";
+
+export function jsxFunction() {
+  let a = 'string';
+  // a = 33;
+
+  return <div>
+    <div>my div</div>
+  </div>;
+}
